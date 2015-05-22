96aea88d0cd7fe28e37ce0d587f78dcedcbeeec0
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-05-22 16:14:14 -0600

    rubocop refactoring

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,25 +1,22 @@
 class Complement
-
   def self.invalid_character(strand, letter)
-      if strand.count(letter) != 0
-        raise ArgumentError
-        puts "Invalid character"
-      end
+    if strand.count(letter) != 0
+      fail ArgumentError
+      puts 'Invalid character'
+    end
   end
 
   def self.of_dna(strand)
-    if invalid_character(strand, "U")
+    if invalid_character(strand, 'U')
     else
       strand.tr('GCTA', 'CGAU')
     end
   end
 
   def self.of_rna(strand)
-    if invalid_character(strand, "T")
+    if invalid_character(strand, 'T')
     else
       strand.tr('GCAU', 'CGTA')
     end
   end
 end
-
-
