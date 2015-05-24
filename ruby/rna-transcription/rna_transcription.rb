3b9b5c0192eea9a8e5b4b3500ce1469587b23b3e
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-05-24 14:20:42 -0600

    second iteration

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,22 +1,43 @@
+# Converting and returning DNA/RNA complements
 class Complement
+  @complements =
+  {
+    'G' => 'C',
+    'C' => 'G',
+    'T' => 'A',
+    'A' => 'U'
+  }
+
+  @rna_complements = @complements.invert
+
   def self.invalid_character(strand, letter)
-    if strand.count(letter) != 0
-      fail ArgumentError
-      puts 'Invalid character'
+    fail ArgumentError, 'Invalid character.' if strand.count(letter) != 0
+  end
+
+  def self.convert_strands(strand, hash)
+    strand.gsub(strand, hash)
+  end
+
+  def self.return_complement(strand, hash)
+    new_strand = []
+    strand = strand.split('')
+    strand.each do |single_strand|
+      new_strand << convert_strands(single_strand, hash)
     end
+    new_strand.join
   end
 
   def self.of_dna(strand)
     if invalid_character(strand, 'U')
     else
-      strand.tr('GCTA', 'CGAU')
+      return_complement(strand, @complements)
     end
   end
 
   def self.of_rna(strand)
     if invalid_character(strand, 'T')
     else
-      strand.tr('GCAU', 'CGTA')
+      return_complement(strand, @rna_complements)
     end
   end
 end
