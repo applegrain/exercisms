626b6551335670a9de40922ce2fa57aa98caeea0
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-06-15 07:36:07 -0600

    second iteration - lines 9-11 are smelly

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -1,24 +1,25 @@
 class Prime
 
-  def self.nth(num)
-    array = []
-    int = 2
-    verify_input(num)
-
-    until array.size == num
-      array << int if self.is_prime?(int)
-      int += 1
-    end
-    array.last
-  end
-
-  def self.is_prime?(int)
-    limit = int / 2
-    (2..limit).none? { |x| int % x == 0 }
-  end
-
   def verify_input(num)
     fail ArgumentError if num == 0
     "0 is not a prime number."
   end
+
+  def self.nth(num)
+    verify_input(num)
+    primes = [2]
+    int = 3
+
+    until primes.size == num
+      primes << int if self.prime?(int)
+      int += 1
+    end
+    primes.last
+  end
+
+  def self.prime?(int)
+    limit = Math.sqrt(int).ceil
+    (2..limit).none? { |x| int % x == 0 }
+  end
+
 end
