34532327188755a0c0c00baaf815a0f7539e50ca
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-07-09 13:04:42 -0600

    first iteration food chain

--- ruby/food-chain/food_chain.rb
+++ ruby/food-chain/food_chain.rb
@@ -1,28 +1,37 @@
 class FoodChain
 
+  def self.song
+    [*0..7].map do |i|
+      verse(i) + "\n"
+    end.join("\n")
+  end
+
   def self.animals
      [
       {animal: "fly"},
       {animal: "spider", description: "It wriggled and jiggled and tickled inside her."},
-      {animal: "bird", description: "How absurd to swallow a bird!"}
-      # {animal => cat}
-      # {animal => dog}
-      # {animal => goat}
-      # {animal => cow}
-      # {animal => horse}
+      {animal: "bird", description: "How absurd to swallow a bird!"},
+      {animal: "cat", description: "Imagine that, to swallow a cat!"},
+      {animal: "dog", description: "What a hog, to swallow a dog!"},
+      {animal: "goat", description: "Just opened her throat and swallowed a goat!"},
+      {animal: "cow", description: "I don't know how she swallowed a cow!"},
+      {animal: "horse"}
     ]
   end
+
   def self.verse(index)
     current = animals[index]
+
+    return last_verse(current) if current[:animal] == "horse"
     opening = self.opening(current[:animal])
     closing = self.closing
 
-    [
+    arr = [
       opening,
       description(current),
       generate_middle_part(index),
       closing
-    ].reject(&:empty?).join("\n")
+    ].flatten.reject(&:empty?).join("\n")
   end
 
   def self.description(current)
@@ -30,14 +39,25 @@ class FoodChain
   end
 
   def self.generate_middle_part(index)
-    lines = (0...index).map do |index|
+    lines = [*0..index].reverse.map do |index|
       self.swallowed_animal(animals[index][:animal], animals[index-1][:animal])
     end
-    lines.empty? ? "" : lines
+    verse = lines.empty? ? "" : lines
+    add_spider_description(verse)
   end
 
   def self.swallowed_animal(current, previous)
-    current == "fly" ? "" : "She swallowed the #{current} to catch the #{previous}."
+    verse = current == "fly" ? "" : "She swallowed the #{current} to catch the #{previous}."
+  end
+
+  def self.add_spider_description(middle_verse)
+    middle_verse.map do |line|
+      if line[36..-2] == ("spider")
+        line.chop + " that wriggled and jiggled and tickled inside her."
+      else
+        line
+      end
+    end
   end
 
   def self.opening(animal)
@@ -46,5 +66,10 @@ class FoodChain
 
   def self.closing
     "I don't know why she swallowed the fly. Perhaps she'll die."
+  end
+
+  def self.last_verse(current)
+    "I know an old lady who swallowed a #{current[:animal]}.
+She's dead, of course!"
   end
 end
--- ruby/food-chain/food_chain_test.rb
+++ ruby/food-chain/food_chain_test.rb
@@ -6,16 +6,7 @@ require 'minitest/pride'
 require_relative 'food_chain'
 
 class FoodChainTest < Minitest::Test
-  # This test is an acceptance test.
-  #
-  # If you find it difficult to work the problem with so much
-  # output, go ahead and add a `skip`, and write whatever
-  # unit tests will help you. Then unskip it again
-  # to make sure you got it right.
-  # There's no need to submit the tests you write, unless you
-  # specifically want feedback on them.
   def test_the_whole_song
-    skip
     song_file = File.expand_path('../song.txt', __FILE__)
     expected = IO.read(song_file)
     assert_equal expected, FoodChain.song
@@ -59,7 +50,64 @@ She swallowed the bird to catch the spider that wriggled and jiggled and tickled
 She swallowed the spider to catch the fly.
 I don't know why she swallowed the fly. Perhaps she'll die.}
     index = 2
-    assert_equal expected, FoodChain.verse(index )
+    assert_equal expected, FoodChain.verse(index)
+  end
+
+  def test_it_generates_the_fourth_verse
+    expected =%{I know an old lady who swallowed a cat.
+Imagine that, to swallow a cat!
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.}
+    index = 3
+    assert_equal expected, FoodChain.verse(index)
+  end
+
+  def test_it_generates_the_fifth_verse
+    expected =%{I know an old lady who swallowed a dog.
+What a hog, to swallow a dog!
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.}
+    index = 4
+    assert_equal expected, FoodChain.verse(index)
+  end
+
+  def test_it_generates_the_sixth_verse
+    expected =%{I know an old lady who swallowed a goat.
+Just opened her throat and swallowed a goat!
+She swallowed the goat to catch the dog.
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.}
+    index = 5
+    assert_equal expected, FoodChain.verse(index)
+  end
+
+  def test_it_generates_the_seventh_verse
+    expected =%{I know an old lady who swallowed a cow.
+I don't know how she swallowed a cow!
+She swallowed the cow to catch the goat.
+She swallowed the goat to catch the dog.
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.}
+    index = 6
+    assert_equal expected, FoodChain.verse(index)
+  end
+
+  def test_it_generates_the_eigth_verse
+    expected =%{I know an old lady who swallowed a horse.
+She's dead, of course!}
+    index = 7
+    assert_equal expected, FoodChain.verse(index)
   end
 
   # This is some simple book-keeping to let people who are
