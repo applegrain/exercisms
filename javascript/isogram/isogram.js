e43cce9a54dea7d5018c99ab8a1f3b40ea0fed30
Lovisa Svallingson <lovisa.svallingson@ca.com>
2016-07-24 11:57:50 -0600

    Isogram: iteration 2
    TODO: figure out a way to generate an array with all alphanumeric characters

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -3,27 +3,22 @@
 class Isogram {
   constructor(word) {
     this.word = this._sanitize(word);
+    // TODO: this array is a disaster. should instead generate an array with all alphanumeric chars
+    this.alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                     't', 'u', 'v', 'w', 'x', 'y', 'z', 'é', 'å', 'ä', 'ö', 'ü', 'ß'];
   }
 
   isIsogram() {
-    let categorized = this.categorize();
+    for (let i = 0; i < this.word.length; i++) {
+      let index = this.alphabet.indexOf(this.word[i]);
+      if (index === -1) { return false; }
 
-    return Object.keys(categorized).filter(function(el) {
-      return categorized[el] > 1;
-    }).length === 0;
+      this.alphabet.splice(index, 1);
+    }
+    return true;
   }
-
-  categorize() {
-    return this.word.reduce(function(hash, letter) {
-      if (!hash.hasOwnProperty(letter)) {
-        hash[letter] = 0;
-      }
-      hash[letter] = hash[letter] + 1;
-      return hash;
-    }, {});
-  }
-
   _sanitize(input) {
+    // TODO: the replace()'s are too fragile
     return input.toLowerCase().replace(/ /g,'').replace(/-/g, '').split('');
   }
 };
