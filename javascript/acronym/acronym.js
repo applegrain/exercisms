df66c72a2c6268e0f372b832d6408a9939ac7a1c
Lovisa Svallingson <lovisa.svallingson@ca.com>
2016-11-10 16:04:20 -0700

    Clean up a lil bit

--- javascript/acronym/acronym.js
+++ javascript/acronym/acronym.js
@@ -1,13 +1,14 @@
 module.exports = {
-  parse: function(string) {
-    return string.split(/[^0-9a-zA-Z]/g).reduce(function(acronym, substr) {
-
+  parse(string) {
+    return string.split(/[^a-zA-Z]/g).reduce((acronym, substr) => {
 
       if (/[A-Z]/.test(substr)) {
-        acronym += substr.split(/[a-z]/).filter(function(character) {
-          return character;
-        }).map(function(char) { return char[0] }).join('');
-      } else if (substr) {
+        acronym += substr.split(/[a-z]/)
+                         .filter(character => character)
+                         .map(character => character[0])
+                         .join('');
+      }
+      else if (substr) {
         acronym += substr[0].toUpperCase();
       }
 
